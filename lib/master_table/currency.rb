ba392b5a504a55b5a02567f6a5e908915483d449
suite_selection <egami2787@gmail.com>
2013-03-17 19:22:56 +0900

    facebook

---
+++ lib/master_table/currency.rb
@@ -0,0 +1,9 @@
+# encoding: utf-8
+module MasterTable
+  module Currency
+    Options = {
+      "￥" => 1,
+      "$"  => 2
+    }
+  end
+end
---
+++ lib/master_table/language.rb
@@ -0,0 +1,9 @@
+# encoding: utf-8
+module MasterTable
+  module Language
+    Options = {
+      "日本語" => 1,
+      "English"  => 2
+    }
+  end
+end
