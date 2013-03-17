00c1660d4f333de97cfadee84dc17f3fe391d334
suite_selection <egami2787@gmail.com>
2013-03-18 01:10:34 +0900

    user,language

--- lib/master_table/language.rb
+++ lib/master_table/language.rb
@@ -1,9 +1,11 @@
 # encoding: utf-8
 module MasterTable
   module Language
+    JAPANESE = 1
+    ENGLISH  = 2
     Options = {
-      "日本語" => 1,
-      "English"  => 2
+      "日本語" => JAPANESE,
+      "English"  => ENGLISH
     }
   end
 end
