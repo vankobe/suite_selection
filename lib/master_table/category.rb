64efa1ba17d41b67c401673d5fc658438255c5ae
suite_selection <egami2787@gmail.com>
2013-03-20 21:10:45 +0900

    add category

---
+++ lib/master_table/category.rb
@@ -0,0 +1,13 @@
+# encoding: utf-8
+module MasterTable
+  module Category
+    NAME = {
+      1 => "チョコレート",
+      2 => "ケーキ",
+      3 => "焼き菓子",
+      4 => "生菓子",
+      5 => "キャンディー", 
+      6 => "その他"
+    }
+  end
+end
