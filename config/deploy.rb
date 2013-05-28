804fc6142950000778fd813eaeb9d797acf8700c
suite_selection <egami2787@gmail.com>
2013-05-28 23:47:41 +0900

    deploy file

---
+++ config/deploy.rb
@@ -0,0 +1,35 @@
+set :application, "sweeta"
+set :repository,  "ssh://49.212.221.48:/home/egami_masato/git/sweeta.git"
+
+set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :branch, "master"
+set :deploy_to, "/var/www/#{application}"
+
+role :web, "49.212.221.48"                          # Your HTTP server, Apache/etc
+role :app, "49.212.221.48"                          # This may be the same as your `Web` server
+role :db,  "49.212.221.48", :primary => true # This is where Rails migrations will run
+#role :db,  "49.212.221.48"
+
+# ssh
+set :user, "app"
+set :ssh_options, :port => 22, :forward_agent => false, :keys => "/home/egami_masato/.ssh/id_dsa", :passphrase => "2Gyouza7"
+set :use_sudo, false
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+task :list do
+  run "ls"
+end
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
